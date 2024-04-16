030a07e4892752c5db974a97733d634e8bd69041
arethefreshest <doffen.are@gmail.com>
2024-04-16 15:34:47 +0200

    fix merge

--- App.js
+++ App.js
@@ -1,27 +1,6 @@
-/*import React from "react";
+import React from 'react';
 import AppNavigator from "./AppNavigator";
 
 export default function App() {
-  return <AppNavigator />;
-}*/
-
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text } from 'react-native';
-
-import Activities from './components/Activities'
-import ActivityFilter from './components/ActivityFilter'
-
-const Stack = createNativeStackNavigator();
-
-export default function App() {
-  return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Filter" component={ActivityFilter} />
-          <Stack.Screen name="Activities" component={Activities} />
-        </Stack.Navigator>
-      </NavigationContainer>
-  );
+  return <AppNavigator/>;
 }
