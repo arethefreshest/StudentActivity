42ce82ebfedc1564114e8d448fd4db091e204f7d
David Bratland Holt <davidbh@uia.no>
2024-04-16 15:24:59 +0200

    Startet Basic layout med bottom-meny

--- App.js
+++ App.js
@@ -6,18 +6,11 @@ export default function App() {
 }*/
 
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text } from 'react-native';
-
-import Activities from './components/Activities'
-import ActivityFilter from './components/ActivityFilter'
 import AppNavigator from "./AppNavigator";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <AppNavigator/>
-  );
+  return <AppNavigator/>;
 }
