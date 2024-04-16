6fdaa6c213fbb251792c6fb4b258decb8910b74c
mini <olesb@uia.no>
2024-04-16 12:52:51 +0200

    Firebase

---
+++ FirebaseConfig.js
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCZEPuwt1QvvdyIQ_c09F068WBGbpP0Cg0",
+    authDomain: "lifesaver-419c4.firebaseapp.com",
+    projectId: "lifesaver-419c4",
+    storageBucket: "lifesaver-419c4.appspot.com",
+    messagingSenderId: "111694861959",
+    appId: "1:111694861959:web:69695be7db75cfd27a8a22",
+    measurementId: "G-HNMLKWJ6YK"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Initialize Analytics
+isSupported().then((supported) => {
+    if (supported) {
+        const analytics = getAnalytics(app);
+    } else {
+        console.log('Firebase Analytics not supported in this environment');
+    }
+});
+
+// Initialize Auth with persistence
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export { app, auth, db };
