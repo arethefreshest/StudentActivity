e88cbbbd2f5e0e2f9f216b13f1ad42bc7b4b86cb
David Bratland Holt <davidbh@uia.no>
2024-04-16 15:36:39 +0200

    Cleanup App.js

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
-/*import React from "react";
-import AppNavigator from "./AppNavigator";
-
-export default function App() {
-  return <AppNavigator />;
-}*/
-
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AppNavigator from "./AppNavigator";
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return <AppNavigator/>;
