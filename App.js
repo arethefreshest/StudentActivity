7077a5f7e9fff0452910a58ca25e4d96380fae74
David Bratland Holt <davidbh@uia.no>
2024-04-16 12:56:41 +0200

    Initial commit, broken

--- App.js
+++ App.js
@@ -1,6 +1,20 @@
-import React from "react";
-import AppNavigator from "./AppNavigator";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text } from 'react-native';
+
+import Activities from './components/Activities'
+import ActivityFilter from './components/ActivityFilter'
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Filter" component={ActivityFilter} />
+          <Stack.Screen name="Activities" component={Activities} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
 }
