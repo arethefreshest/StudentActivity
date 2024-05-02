b5106d81ddf37696c4592424b957c92f768824e1
arethefreshest <doffen.are@gmail.com>
2024-05-02 15:02:04 +0200

    Second screen done with components, missing functionality only to Firebase) + Added .js for svg icon to email

--- components/ThirdPartyIconRow.js
+++ components/ThirdPartyIconRow.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { View, TouchableOpacity } from "react-native";
-import { styles } from "../styles";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
+import { styles as globalStyles } from "../styles";
 import GoogleLogo from "../assets/GoogleLogo";
 import AppleLogo from "../assets/AppleLogo";
 import OutlookLogo from "../assets/OutlookLogo";
 import FacebookLogo from "../assets/FacebookLogo";
 
-const ThirdPartyIconRow = ({ onPressGoogle, onPressApple, onPressOutlook, onPressFacebook }) => {
+const ThirdPartyIconRow = ({ onPressGoogle, onPressApple, onPressOutlook, onPressFacebook, style }) => {
+    const mergedStyles = StyleSheet.flatten([globalStyles.thirdPartyRow, style]);
+
     return (
-        <View style={styles.thirdPartyRow}>
-            <TouchableOpacity onPress={onPressGoogle} style={[styles.thirdPartyIcon, { left: 96 }]}>
+        <View style={mergedStyles}>
+            <TouchableOpacity onPress={onPressGoogle} style={[globalStyles.thirdPartyIcon, { left: 95 }]}>
                 <GoogleLogo  />
             </TouchableOpacity>
-            <TouchableOpacity onPress={onPressApple} style={[styles.thirdPartyIcon, { left: 96 + 48 + 14.67 }]}>
+            <TouchableOpacity onPress={onPressApple} style={[globalStyles.thirdPartyIcon, { left: 96 + 48 + 14.67 }]}>
                 <AppleLogo />
             </TouchableOpacity>
-            <TouchableOpacity onPress={onPressOutlook} style={[styles.thirdPartyIcon, { left: 96 + (48 + 14.67) * 2 }]}>
+            <TouchableOpacity onPress={onPressOutlook} style={[globalStyles.thirdPartyIcon, { left: 96 + (48 + 14.67) * 2 }]}>
                 <OutlookLogo />
             </TouchableOpacity>
-            <TouchableOpacity onPress={onPressFacebook} style={[styles.thirdPartyIcon, { left: 96 + (48 + 14.67) * 3 }]}>
+            <TouchableOpacity onPress={onPressFacebook} style={[globalStyles.thirdPartyIcon, { left: 96 + (48 + 14.67) * 3 }]}>
                 <FacebookLogo />
             </TouchableOpacity>
         </View>
