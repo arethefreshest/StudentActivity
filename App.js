ab44fa1fd09a850692128502e4b9d2336fc132d1
David Holt <davidbh@uia.no>
2024-05-07 21:51:51 +0200

    ye

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppNavigator from "./AppNavigator";
+import GoogleMaps from "./components/GoogleMaps";
 
 export default function App() {
-  return <AppNavigator/>;
+  return <GoogleMaps />
 }
