cb1934101b007573875466bbe0c1e37f223f26c2
mini <olesb@uia.no>
2024-05-16 22:52:32 +0200

    git push

--- AppNavigator.js
+++ AppNavigator.js
@@ -45,7 +45,7 @@ function AppNavigator({ isAuthenticated }) {
     }
 
 
-    }
+
 
     function Social() {
         return (
@@ -62,7 +62,8 @@ function AppNavigator({ isAuthenticated }) {
             </Stack.Navigator>
         );
     }
-        return (
+
+return (
             <MaterialBottomTabs.Navigator
                 barStyle={{
                     backgroundColor: 'transparent',
@@ -143,82 +144,5 @@ function AppNavigator({ isAuthenticated }) {
                 />
             </MaterialBottomTabs.Navigator>
         );
-
-
-    return (
-        <MaterialBottomTabs.Navigator
-            barStyle={{
-                backgroundColor: 'transparent',
-                borderTopWidth: 0,
-                borderColor: 'transparent',
-                position: 'absolute',
-                bottom: 0,
-                elevation: 0,
-                shadowOffset: { height: 0, width: 0 },
-                shadowOpacity: 0,
-                height: 104,
-            }}
-            screenOptions={({ route }) => ({
-                tabBarIcon: ({ focused }) => {
-                    let iconName;
-                    let iconColor = focused ? "#FFECE7" : "#9DB3B3";
-                    switch (route.name) {
-                        case "Home":
-                            iconName = <HjemIkon fill={iconColor} />;
-                            break;
-                        case "Social":
-                            iconName = <LeggTilIkon fill={iconColor} />;
-                            break;
-                        case "Add":
-                            iconName = <LeggTilIkon fill={iconColor} />;
-                            break;
-                        case "Profil":
-                            iconName = <ProfilIkon fill={iconColor} stroke={iconColor} strokeWidth={focused ? 2.33333 : 0} />;
-                            break;
-                    }
-                    return iconName;
-                },
-                tabBarLabelStyle: { color: '#9DB3B3' },
-                tabBarActiveTintColor: '#FFECE7',
-                tabBarInactiveTintColor: '#9DB3B3',
-                tabBarStyle: {
-                    borderTopWidth: 0,
-                    shadowColor: 'transparent',
-                    elevation: 0,
-                    backgroundColor: 'transparent'
-                },
-                headerStyle: {
-                    borderBottomWidth: 0,
-                    shadowColor: 'transparent',
-                    elevation: 0,
-                }
-            })}
-        >
-            <MaterialBottomTabs.Screen
-                name="Home"
-                component={Home}
-                options={{ tabBarLabel: 'Hjem' }}
-            />
-            <MaterialBottomTabs.Screen
-                name="Social"
-                component={Social}
-                options={{ tabBarLabel: 'Sosialt' }}
-            />
-            <MaterialBottomTabs.Screen
-                name="Add"
-                component={AddActivity}
-                options={{ tabBarLabel: 'Legg til' }}
-            />
-            <MaterialBottomTabs.Screen
-                name="Profil"
-                component={ProfilNavigator}
-                options={{
-                    tabBarLabel: 'Profil',
-                    unmountOnBlur: true
-                }}
-            />
-        </MaterialBottomTabs.Navigator>
-    );
 }
-
 export default AppNavigator;
