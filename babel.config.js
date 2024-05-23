ade56cb6a2d198fc93b5049a6acc01e12efcdd58
selvi <K@>
2024-05-23 14:58:05 +0200

    built and added to playstore

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['module:react-native-dotenv', {
-        moduleName: '@env',
-        path: '.env',
-        blacklist: null,
-        whitelist: null,
-        safe: false,
-        allowUndefined: true
-    }]
-  ],
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+        ['module:react-native-dotenv', {
+            moduleName: '@env',
+            path: '.env',
+            blacklist: null,
+            whitelist: null,
+            safe: false,
+            allowUndefined: true
+        }],
+        ['@babel/plugin-transform-class-properties', { loose: true }],
+        ['@babel/plugin-transform-private-methods', { loose: true }],
+        ['@babel/plugin-transform-private-property-in-object', { loose: true }]
+    ],
 };
